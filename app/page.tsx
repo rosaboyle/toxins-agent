ee9b8dd53d944fb1792c6a2b75421c7b1352722b
Dheeraj Pai <helloworldcmu@gmail.com>
2024-11-21 19:54:36 -0800

    Updates on UI

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
-import { toast, useToast } from "@/components/ui/use-toast"
+import { toast, useToast } from "@/hooks/use-toast"
 import { useRouter } from 'next/navigation'
 
 export default function TextInputAndUploadPage() {
